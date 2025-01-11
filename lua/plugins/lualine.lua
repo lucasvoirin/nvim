b2591ccd31bda2b1c08b966616348f7ff6e962d0
lucasvoirin <lucas.voirin@mailo.com>
2025-01-11 13:00:48 -0500

    Fixing lualine in terminal

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,6 +1,13 @@
 return {
   'nvim-lualine/lualine.nvim',
   opts = {
+    options = {
+      component_separators = { left = "｜", right = "｜" },
+      section_separators = { left = " ", right = " " },
+      disabled_filetypes = {
+      statusline = {"snacks_dashboard"},
+    },
+    },
     sections = {
       lualine_c = {
 	{ "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 }},
@@ -16,7 +23,30 @@ return {
       },
       lualine_y = {},
       lualine_z = {
-	{ "location", separator = " /", padding = { left = 0, right = 0 } }, "vim.fn.line('$')"
+        {
+          function() 
+            local line = vim.fn.line('.')
+            local col = vim.fn.col('.')
+            local total_lines = vim.fn.line('$')
+            return string.format("%d:%d / %d", line, col, total_lines)
+          end,
+          separator = " / ",
+          cond = function() return vim.bo.buftype ~= "terminal" and vim.bo.filetype ~= "snacks_dashboard" end
+        }
+      },
+    },
+    inactive_sections = {
+      lualine_c = {
+	{
+	  'filename',
+	  path = 3
+	}
+      },
+      lualine_x = {
+	{
+	  'location',
+	  cond = function() return vim.bo.buftype ~= "terminal" and vim.bo.filetype ~= "snacks_dashboard" end
+        }
       },
     },
   },
--- lua/plugins/quarto.lua
+++ lua/plugins/quarto.lua
@@ -1,9 +1,7 @@
 return {
-  {
-    "quarto-dev/quarto-nvim",
-    dependencies = {
-      "jmbuhr/otter.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
+  "quarto-dev/quarto-nvim",
+  dependencies = {
+    "jmbuhr/otter.nvim",
+    "nvim-treesitter/nvim-treesitter",
   },
 }
